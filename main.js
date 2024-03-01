ea04fb8ea9ee6ad3b51c220e2ab002e8990dce1b
MuhammadFahadgithub <159857192+MuhammadFahadgithub@users.noreply.github.com>
2024-03-01 16:09:09 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+// Task21
+// Write a program that creates objects containing these items.
+var person = {
+    name: 'Jahangeer',
+    age: 27,
+    nationality: 'Pakistan',
+    Student: true
+};
+// Data type of object
+console.log(person);
+//  car object
+var car = {
+    maker: 'Honda',
+    color: 'White',
+    automatic: true
+};
+console.log(car);
---
+++ main.ts
@@ -0,0 +1,32 @@
+// Task21
+// Write a program that creates objects containing these items.
+
+
+interface person {
+    name : string,
+    age: number,
+    nationality: string,
+    Student : Boolean
+}
+let person : person = {
+    name : 'Jahangeer',
+    age : 27,
+    nationality: 'Pakistan',
+    Student : true
+
+}
+
+// Data type of object
+console.log(person);
+interface car {
+    maker : string,
+    color: string,
+    automatic: boolean
+}
+//  car object
+let car = {
+maker : 'Honda',
+color: 'White',
+automatic : true
+}
+console.log (car);
